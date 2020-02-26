1ebfe380c6014df9fcd42d72d8af20360afd67d6
Jayshree Anandakumar <ping.jayshree@gmail.com>
2020-02-26 19:12:08 +0530

    Context on click action

--- content.js
+++ content.js
@@ -28,3 +28,26 @@ chrome.storage.sync.get(null, function(items) {
   // var id = chrome.contextMenus.create({"id": "tr_options", "title": "Edit as deal to taghash", "parentId": parent, "contexts":["selection"]});
   
 });
+
+chrome.contextMenus.onClicked.addListener(function(info, tab) {
+  if (info.menuItemId == "tr_parent") {
+    click(info, tab);
+  } else {
+    // click(info, tab);
+  }
+});
+
+function click(info, tab) {
+  chrome.windows.create({
+    type: 'panel', url: 'https://translate.google.com/#auto/en'+'/'+ encodeURIComponent(info.selectionText), width: 1000, height: 382}, function(tab) {
+      translator_window = tab.windowId;
+      translator_tab = tab.id;
+      chrome.windows.onRemoved.addListener(function (windowId) {
+        if (windowId == translator_window) {
+          translator_window = false;
+          translator_tab = false;
+        }
+      });
+    }
+  );
+}
