0d257b8d46e26b22c139c9de9581d6b2834c563d
Jayshree Anandakumar <ping.jayshree@gmail.com>
2020-02-26 14:14:18 +0530

    New deal form; added storage permissions

--- content.js
+++ content.js
@@ -8,3 +8,15 @@
 //     }
 //   }
 // );
+
+
+
+// Save it using the Chrome extension storage API.
+chrome.storage.sync.set({'foo': 'hello', 'bar': 'hi'}, function() {
+  console.log('Settings saved');
+});
+
+// Read it using the storage API
+chrome.storage.sync.get(['foo', 'bar'], function(items) {
+  message('Settings retrieved', items);
+});
