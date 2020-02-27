7b5c2381a90f98f42bb133474bfdf5e7b115c433
Jayshree Anandakumar <ping.jayshree@gmail.com>
2020-02-27 18:40:01 +0530

    Create deals API integeration

--- content.js
+++ content.js
@@ -25,16 +25,38 @@ chrome.contextMenus.onClicked.addListener(function(info, tab) {
 });
 
 function click(info, tab) {
-  chrome.windows.create({
-    type: 'panel', url: 'https://translate.google.com/#auto/en'+'/'+ encodeURIComponent(info.selectionText), width: 1000, height: 382}, function(tab) {
-      translator_window = tab.windowId;
-      translator_tab = tab.id;
-      chrome.windows.onRemoved.addListener(function (windowId) {
-        if (windowId == translator_window) {
-          translator_window = false;
-          translator_tab = false;
-        }
-      });
-    }
-  );
+
+  // chrome.windows.create({
+  //   type: 'panel', url: 'https://translate.google.com/#auto/en'+'/'+ encodeURIComponent(info.selectionText), width: 1000, height: 382}, function(tab) {
+  //     translator_window = tab.windowId;
+  //     translator_tab = tab.id;
+  //     chrome.windows.onRemoved.addListener(function (windowId) {
+  //       if (windowId == translator_window) {
+  //         translator_window = false;
+  //         translator_tab = false;
+  //       }
+  //     });
+  //   }
+  // );
+  
+  // var data = {
+  //   "startup": { "name": "api company test" },
+  //   "founder": {"user": {"email_primary": "founder@test.com" }}
+  // }
+  // // http://dev.taghash.com:3000/api/v3/deals
+
+  // fetch('http://dev.taghash.com:3000/api/v3/deals',{ 
+  //   method: 'post',
+  //   headers: {
+  //     'Content-Type': 'application/json'
+  //   },
+  //   referrerPolicy: 'no-referrer', // no-referrer, *client
+  //   body: JSON.stringify(data) 
+  // }).then(r => r.json()).then(result => {
+  //   console.log(result)
+  //   // Result now contains the response text, do what you want...
+  // })
+
+
+  
 }
