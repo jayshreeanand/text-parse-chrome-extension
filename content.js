607bcc9e7522aa3833128f333bbd05fdbf4fbf54
Jayshree Anandakumar <ping.jayshree@gmail.com>
2020-02-27 13:33:18 +0530

    Udpated context

--- content.js
+++ content.js
@@ -1,16 +1,3 @@
-// // content.js
-// chrome.runtime.onMessage.addListener(
-//   function(request, sender, sendResponse) {
-//     if( request.message === "clicked_browser_action" ) {
-//       var firstHref = $("a[href^='http']").eq(0).attr("href");
-
-//       console.log(firstHref);
-//     }
-//   }
-// );
-
-
-
 // Save it using the Chrome extension storage API.
 chrome.storage.sync.set({'foo': 'hello', 'bar': 'hi'}, function() {
   console.log('Settings saved');
