7c6a1d50c9ea46d5343af730b07a4659fd718370
Jayshree Anandakumar <ping.jayshree@gmail.com>
2020-03-04 13:22:52 +0530

    Selection click event

---
+++ js/eventPage.js
@@ -0,0 +1,24 @@
+var menuItem = {
+    "id": "taghash",
+    "title": "taghashDeal",
+    "contexts": ["selection"]
+};
+
+function isValid(value) {
+    // check for validity of selected text
+  return !isNaN(value) && 
+         parseInt(Number(value)) == value && 
+         !isNaN(parseInt(value, 10));
+}
+
+chrome.contextMenus.create(menuItem);
+
+chrome.contextMenus.onClicked.addListener(function(clickData){   
+    if (clickData.menuItemId == "taghash" && clickData.selectionText){    
+        if (isInt(clickData.selectionText)){          
+            chrome.storage.sync.get(['deal','content'], function(content){
+                // save selection in chrome storage
+            });
+        }
+    }
+});
