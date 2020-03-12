ebd78826917ad35460bf72b772e4bb40896ae21c
Jayshree Anandakumar <ping.jayshree@gmail.com>
2020-03-12 20:18:07 +0530

    Removed DOM selection from context

--- js/eventPage.js
+++ js/eventPage.js
@@ -8,22 +8,9 @@ chrome.contextMenus.create(menuItem);
 
 chrome.contextMenus.onClicked.addListener(function(clickData, tab){
   if (clickData.menuItemId == "taghash" && clickData.selectionText){
-    // logger.debug(data);
-
-    // chrome.tabs.executeScript(tab.id, {file: "jquery-3.2.0.min.js"}, function () {
-    //     chrome.tabs.executeScript(tab.id, {file: "jquery-1.10.4-ui.min.js"}, function () {
-    //         chrome.tabs.executeScript(tab.id, {
-    //             file: "getDOM.js",
-    //         });
-    //     });
-    // });
-    //
-    var selection = null;
-    chrome.tabs.executeScript(tab.id, { code: "selection = document.getSelection();" }, function(DOMdata) {
-      alert(JSON.stringify(DOMdata))
-      chrome.storage.sync.set({'content': JSON.stringify(DOMdata) }, function(data){
-      });
-    })
+    chrome.storage.sync.set({'content': clickData.selectionText }, function(data){
+      //logger.debug(data)
+    });
   }
 });
 
