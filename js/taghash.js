1ab94e1e64f8997e9090db93421e9b298c45eb9c
Jayshree Anandakumar <ping.jayshree@gmail.com>
2020-03-12 20:49:10 +0530

    WIP: Handle successfully created deals

--- js/taghash.js
+++ js/taghash.js
@@ -1,34 +1,31 @@
 $('#create-deal-button').click(function(){
   var data = {
-    "startup": { "name": "api company test" },
+    "startup": { "name": "AJ Test" },
     "founder": {"user": {"email_primary": "founder@test.com" }}
   }
 
   let url = config.baseUrl + TaghashURL.DEAL_CREATE.uri;
   const method = TaghashURL.DEAL_CREATE.method;
 
-  authGetAccessToken((accessTokenErr, accessToken) => {
-    if (accessTokenErr) {
-      // handle access token not set
-    }
-
+  chrome.storage.local.get(['access_token'], function(result) {
     fetch(url,{
       method: method,
       headers: {
         ...TaghashURL.HEADERS_COMMON,
-        Authorization: `Bearer ${accessToken}`,
+        Authorization: `Bearer ${result.access_token}`,
       },
       referrerPolicy: 'no-referrer', // no-referrer, *client
       body: JSON.stringify(data)
     }).then(r => r.json()).then(result => {
-      // if (result && result.success && result.data) {
-      //   // Return `user` with callback
-      //   return callback(null, result.data);
-      // } else {
-      //   // unforeseen error handling
-      //   return callback(new Error(result && !result.success ?
-      //     result.message : 'Something went wrong'));
-      // }
-    })
+      if (result && result.success && result.data) {
+        // Return `user` with callback
+         alert("success" + result.data)
+      } else {
+        // unforeseen error handling
+         var error = new Error(result && !result.success ?
+          result.message : 'Something went wrong');
+          alert("failure" + JSON.stringify(result))
+      }
+    });
   });
 });
