83a0bbff39fbac157c565f0e5e4ece3c87e98170
Jayshree Anandakumar <ping.jayshree@gmail.com>
2020-03-12 15:55:31 +0530

    Login handler

--- js/login.js
+++ js/login.js
@@ -1,26 +1,38 @@
 $(function(){
+  // $('#sign-in-button2').click(function(){
+  //   chrome.storage.sync.set({'accessToken': '' }, function(){
+  //     logger.debug("access token reset");
+  //
+  //     login(null, function(token) {
+  //       logger.debug(token)
+  //
+  //       chrome.storage.sync.set({'accessToken': token }, function(){
+  //         logger.debug("access token set");
+  //       });
+  //     });
+  //   });
+
+  var notifOptions = {
+    type: "basic",
+    iconUrl: "images/icon48.png",
+    title: "Login success",
+    message: "You have successfully logged into Taghash"
+  };
+
   $('#sign-in-button').click(function(){
-    chrome.storage.sync.set({'accessToken': '' }, function(){
-      logger.debug("access token reset");
-
-      login(null, function(token) {
-        logger.debug(token)
-
-        chrome.storage.sync.set({'accessToken': token }, function(){
-          logger.debug("access token set");
+    authCheckAuthorization(function(authError, accessToken) {
+      if (authError) { // check for error codes
+        authLogin(function(loginError, accessToken) {
+          if (loginError) {
+            // login failure
+            alert(loginError)
+          } else {
+            chrome.notifications.create('loginNotif', notifOptions);
+          }
         });
-      });
+      } else {
+        chrome.notifications.create('loginNotif', notifOptions);
+      }
     });
-    // login(null, function(token) {
-    //   logger.debug(token)
-    //   var notifOptions = {
-    //     type: "basic",
-    //     iconUrl: "images/icon48.png",
-    //     title: "Login success",
-    //     message: "You have successfully logged into Taghash"
-    //   };
-    //
-    //   chrome.notifications.create('loginNotif', notifOptions);
-    // });
   });
 });
