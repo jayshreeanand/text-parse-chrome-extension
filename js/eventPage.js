8ec0547e483273d5f1d713269e52bdb8db76696b
Jayshree Anandakumar <ping.jayshree@gmail.com>
2020-03-12 15:26:11 +0530

    Save selected text to chrome storage

--- js/eventPage.js
+++ js/eventPage.js
@@ -6,26 +6,15 @@ var menuItem = {
 
 chrome.contextMenus.create(menuItem);
 
-function isValid(value) {
-    // check for validity of selected text
-  return true
-}
-
 chrome.contextMenus.onClicked.addListener(function(clickData){
   if (clickData.menuItemId == "taghash" && clickData.selectionText){
-    if (isValid(clickData.selectionText)){
-      chrome.storage.sync.get(['deal','content'], function(values){
-        // save selection in chrome storage
-
-        chrome.storage.sync.set({'content': selectionText }, function(){
-
-        });
-      });
-    }
+    chrome.storage.sync.set({'content': clickData.selectionText }, function(data){
+      // logger.debug(data);
+    });
   }
 });
 
 
 chrome.storage.onChanged.addListener(function(changes, storageName){
-  chrome.browserAction.setBadgeText({"text": "new"});
+  chrome.browserAction.setBadgeText({"text": "1"});
 });
