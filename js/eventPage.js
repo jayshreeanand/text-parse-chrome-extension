dea375ccb9bfbfec90bf84ffcfb1ebc5fb54ee0f
Jayshree Anandakumar <ping.jayshree@gmail.com>
2020-03-12 18:32:47 +0530

    Fetch DOM elements on contextMenu select

--- js/eventPage.js
+++ js/eventPage.js
@@ -6,14 +6,25 @@ var menuItem = {
 
 chrome.contextMenus.create(menuItem);
 
-chrome.contextMenus.onClicked.addListener(function(clickData){
+chrome.contextMenus.onClicked.addListener(function(clickData, tab){
   if (clickData.menuItemId == "taghash" && clickData.selectionText){
-    chrome.storage.sync.set({'content': clickData }, function(data){
-      // logger.debug(data);
-    });
+    // logger.debug(data);
+
+    // chrome.tabs.executeScript(tab.id, {file: "jquery-3.2.0.min.js"}, function () {
+    //     chrome.tabs.executeScript(tab.id, {file: "jquery-1.10.4-ui.min.js"}, function () {
+    //         chrome.tabs.executeScript(tab.id, {
+    //             file: "getDOM.js",
+    //         });
+    //     });
+    // });
+    //
+    chrome.tabs.executeScript(tab.id, { code: "document.getSelection()" }, function(DOMdata) {
+      alert(JSON.stringify(DOMdata))
+      chrome.storage.sync.set({'content': JSON.stringify(DOMdata) }, function(data){
+      });
+    })
   }
 });
-
 
 chrome.storage.onChanged.addListener(function(changes, storageName){
   chrome.browserAction.setBadgeText({"text": "1"});
---
+++ js/getDOM.js
@@ -0,0 +1,7 @@
+var selection = document.getSelection();
+
+// extract the information you need
+// if needed, return it to the main script with messaging
+function getDOM() {
+  return document.getSelection();
+}
